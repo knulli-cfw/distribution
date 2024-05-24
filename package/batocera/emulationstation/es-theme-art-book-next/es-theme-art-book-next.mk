13dc3c12d8f4c525e49a9430c96be3978c0d2eec
byunjaeil <byunjaeil@gmail.com>
2024-05-24 21:17:34 +0900

    theme fix

--- package/batocera/emulationstation/es-theme-art-book-next/es-theme-art-book-next.mk
+++ package/batocera/emulationstation/es-theme-art-book-next/es-theme-art-book-next.mk
@@ -4,12 +4,12 @@
 #
 ################################################################################
 # Version.: Commits on May 17, 2024
-ES_THEME_ART_BOOK_NEXT_VERSION = 76ab63e24cb1e2258d5e7294b6aa21b4e8f8e98e
-ES_THEME_ART_BOOK_NEXT_SITE = $(call github,anthonycaccese,art-book-next-es,$(ES_THEME_ART_BOOK_NEXT_VERSION))
+ES_THEME_ART_BOOK_NEXT_VERSION = 8a14b1b45fdc41c808ac0c24cf90b0cb0f087d62
+ES_THEME_ART_BOOK_NEXT_SITE = $(call github,UzuCore,es-theme-art-book-dc,$(ES_THEME_ART_BOOK_NEXT_VERSION))
 
 define ES_THEME_ART_BOOK_NEXT_INSTALL_TARGET_CMDS
-    mkdir -p $(TARGET_DIR)/usr/share/emulationstation/themes/es-theme-art-book-next
-    cp -r $(@D)/* $(TARGET_DIR)/usr/share/emulationstation/themes/es-theme-art-book-next
+    mkdir -p $(TARGET_DIR)/usr/share/emulationstation/themes/es-theme-art-book-dc
+    cp -r $(@D)/* $(TARGET_DIR)/usr/share/emulationstation/themes/es-theme-art-book-dc
 endef
 
 $(eval $(generic-package))
