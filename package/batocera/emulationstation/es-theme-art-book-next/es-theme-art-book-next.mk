49ef95c5044e621933ed443c820e612c5f5c0186
byunjaeil <byunjaeil@gmail.com>
2024-05-24 21:11:11 +0900

    Revert " theme update"
    This reverts commit 6a81b665af8e4a7dc99fb0f0a9e4ac4b3a5d2a32.

--- package/batocera/emulationstation/es-theme-art-book-next/es-theme-art-book-next.mk
+++ package/batocera/emulationstation/es-theme-art-book-next/es-theme-art-book-next.mk
@@ -4,12 +4,12 @@
 #
 ################################################################################
 # Version.: Commits on May 10, 2024
-ES_THEME_ART_BOOK_NEXT_VERSION = 8a14b1b45fdc41c808ac0c24cf90b0cb0f087d62
-ES_THEME_ART_BOOK_NEXT_SITE = $(call github,UzuCore,es-theme-art-book-dc,$(ES_THEME_ART_BOOK_NEXT_VERSION))
+ES_THEME_ART_BOOK_NEXT_VERSION = 2e775231c341ef6a7859beb052a3835cf64e3870
+ES_THEME_ART_BOOK_NEXT_SITE = $(call github,anthonycaccese,art-book-next-es,$(ES_THEME_ART_BOOK_NEXT_VERSION))
 
 define ES_THEME_ART_BOOK_NEXT_INSTALL_TARGET_CMDS
-    mkdir -p $(TARGET_DIR)/usr/share/emulationstation/themes/es-theme-art-book-dc
-    cp -r $(@D)/* $(TARGET_DIR)/usr/share/emulationstation/themes/es-theme-art-book-dc
+    mkdir -p $(TARGET_DIR)/usr/share/emulationstation/themes/es-theme-art-book-next
+    cp -r $(@D)/* $(TARGET_DIR)/usr/share/emulationstation/themes/es-theme-art-book-next
 endef
 
 $(eval $(generic-package))
