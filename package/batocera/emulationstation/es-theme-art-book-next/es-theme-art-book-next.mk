1c6f4246d55fb4510e00c5d84093b726c78eaf58
byunjaeil <byunjaeil@gmail.com>
2024-05-25 19:04:19 +0900

    Update es-theme-art-book-next.mk

--- package/batocera/emulationstation/es-theme-art-book-next/es-theme-art-book-next.mk
+++ package/batocera/emulationstation/es-theme-art-book-next/es-theme-art-book-next.mk
@@ -8,8 +8,8 @@ ES_THEME_ART_BOOK_NEXT_VERSION = aa26b24acf9b8b32a35de308d6ab31acf1ad2382
 ES_THEME_ART_BOOK_NEXT_SITE = $(call github,UzuCore,es-theme-art-book-dc,$(ES_THEME_ART_BOOK_NEXT_VERSION))
 
 define ES_THEME_ART_BOOK_NEXT_INSTALL_TARGET_CMDS
-    mkdir -p $(TARGET_DIR)/usr/share/emulationstation/themes/es-theme-art-book-dc
-    cp -r $(@D)/* $(TARGET_DIR)/usr/share/emulationstation/themes/es-theme-art-book-dc
+    mkdir -p $(TARGET_DIR)/usr/share/emulationstation/themes/es-theme-art-book-next
+    cp -r $(@D)/* $(TARGET_DIR)/usr/share/emulationstation/themes/es-theme-art-book-next
 endef
 
 $(eval $(generic-package))
