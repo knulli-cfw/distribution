69b1b1264da3fd93391c93926f5f71ff02e426d0
byunjaeil <byunjaeil@gmail.com>
2024-05-27 23:27:17 +0900

    fix

--- package/batocera/emulationstation/es-theme-anbernic/es-theme-anbernic.mk
+++ package/batocera/emulationstation/es-theme-anbernic/es-theme-anbernic.mk
@@ -1,12 +1,12 @@
 ################################################################################
 #
-# EmulationStation theme "Anbernic"
+# EmulationStation theme "Anbernic DC"
 #
 ################################################################################
-ES_THEME_ANBERNIC_VERSION = e70d44ce686cf64e9fed3b8133b4658e4aaef0fc
-ES_THEME_ANBERNIC_SITE = $(call github,UzuCore,es-theme-anbernic-dc,$(ES_THEME_ANBERNIC_VERSION))
+ES_THEME_ANBERNIC_DC_VERSION = e70d44ce686cf64e9fed3b8133b4658e4aaef0fc
+ES_THEME_ANBERNIC_DC_SITE = $(call github,UzuCore,es-theme-anbernic-dc,$(ES_THEME_ANBERNIC_DC_VERSION))
 
-define ES_THEME_ANBERNIC_INSTALL_TARGET_CMDS
+define ES_THEME_ANBERNIC__DC_INSTALL_TARGET_CMDS
     mkdir -p $(TARGET_DIR)/usr/share/emulationstation/themes/es-theme-anbernic-dc
     cp -r $(@D)/* $(TARGET_DIR)/usr/share/emulationstation/themes/es-theme-anbernic-dc
 endef
