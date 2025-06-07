fd33aecf23a5744972feb775ee0446dba3b85af2
Chrizzo <chrizzo@unterderbruecke.de>
2025-06-08 01:02:24 +0200

    Added 16:9 GBA bezel to default-knulli-sp to make it work on HDMI

--- package/knulli/retroarch/knulli-bezels/knulli-bezels.mk
+++ package/knulli/retroarch/knulli-bezels/knulli-bezels.mk
@@ -3,8 +3,8 @@
 # knulli bezels
 #
 ################################################################################
-# Version.: Commits on June 2, 2025
-KNULLI_BEZELS_VERSION = c0c7e950acebcb29f832cb748d63b8935f01cde9
+# Version.: Commits on June 8, 2025
+KNULLI_BEZELS_VERSION = 391bc8099649b9344afbe007a25c788a6e89fcbb
 KNULLI_BEZELS_SITE = $(call github,chrizzo-hb,knulli-bezels,$(KNULLI_BEZELS_VERSION))
 
 define KNULLI_BEZELS_INSTALL_TARGET_CMDS
