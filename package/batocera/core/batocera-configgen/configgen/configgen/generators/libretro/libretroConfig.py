1ad158df8c574801ffa8577526239181be567cac
Mikhailzrick <90733003+Mikhailzrick@users.noreply.github.com>
2025-07-10 09:36:55 -0500

    change-rewind-granularity
    Defaulting to "2" reduces the performance hit from enabling rewind by a good amount.

--- package/batocera/core/batocera-configgen/configgen/configgen/generators/libretro/libretroConfig.py
+++ package/batocera/core/batocera-configgen/configgen/configgen/generators/libretro/libretroConfig.py
@@ -770,6 +770,7 @@ def createLibretroConfig(generator: Generator, system: Emulator, controllers: Co
     if system.isOptSet('rewind') and system.getOptBoolean('rewind') == True:
         if(not system.name in systemNoRewind):
             retroarchConfig['rewind_enable'] = 'true'
+            retroarchConfig['rewind_granularity'] = '2' # This reduces the rewind performance cost a good amount
         else:
             retroarchConfig['rewind_enable'] = 'false'
     else:
