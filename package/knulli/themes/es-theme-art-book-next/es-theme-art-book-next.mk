87923bf3ed4d97376e2ecfa302951d5212e7258b
Ant <anthonycaccese@gmail.com>
2026-02-25 11:27:44 -0500

    Update theme version for Art Book Next
    Includes:
    - New notification placement
    - New images for upcoming naming changes

--- package/knulli/themes/es-theme-art-book-next/es-theme-art-book-next.mk
+++ package/knulli/themes/es-theme-art-book-next/es-theme-art-book-next.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 # Version.: Commits on December 30, 2024
-ES_THEME_ART_BOOK_NEXT_VERSION = 5db8b08c41be87843cf47e8b9de605ed7d26c81b
+ES_THEME_ART_BOOK_NEXT_VERSION = 2556331de044ea014433667a91b159292bc570eb
 ES_THEME_ART_BOOK_NEXT_SITE = $(call github,anthonycaccese,art-book-next-es,$(ES_THEME_ART_BOOK_NEXT_VERSION))
 
 define ES_THEME_ART_BOOK_NEXT_INSTALL_TARGET_CMDS
